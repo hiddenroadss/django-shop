0e1e047181b58843084707cda81efd200732a78c
taras <hiddenroadss@gmail.com>
2020-12-04 12:22:23 +0200

    upd

--- onlineShop/onlineShop/settings.py
+++ onlineShop/onlineShop/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test',
         'USER': 'taras',
-        'PASSWORD': 'scanoer1996',
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
